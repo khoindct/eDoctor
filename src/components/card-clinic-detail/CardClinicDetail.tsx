6c5b27b15939d7d6f03035b37914ec66641ad7ab
Khoi Nguyen <khoindct123@gmail.com>
2021-05-18 22:51:22 +0700

    add search clinic for client

--- src/components/card-clinic-detail/CardClinicDetail.tsx
+++ src/components/card-clinic-detail/CardClinicDetail.tsx
@@ -1,83 +1,91 @@
+import React from "react";
 import {
   Box,
   Button,
   Card,
+  CardActions,
   CardContent,
-  CardHeader,
+  CardMedia,
+  Collapse,
   Divider,
-  TextField,
-  Typography,
-} from '@material-ui/core';
-import { useNavigate } from 'react-router-dom';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-
-// styles
-import useStyles from './styles';
+  IconButton,
+} from "@material-ui/core";
+import { Rating } from "@material-ui/lab";
+import classnames from "classnames";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { useNavigate } from "react-router-dom";
+import "./CardClinicDetail.scss";
+import Map from "../map/Map";
 
 const CardClinicDetail: React.FC = () => {
-  const classes = useStyles();
   const navigate = useNavigate();
+  const [ratingValue, setRatingValue] = React.useState<number | null>(2);
+  const [expanded, setExpanded] = React.useState(false);
 
-  const handleFormSubmit = () => navigate('/app/customers', { replace: true });
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const navigateToBooking = () => navigate("/app/customers", { replace: true });
 
   return (
-    <Card className={classes.root} variant="outlined">
-      <CardHeader title="Details" />
-      <Divider />
-      <CardContent>
-        <div className={classes.details}>
-          <div className={classes.details_row}>
-            <Typography
-              variant="h4"
-              component="h4"
-              style={{ color: 'rgb(23, 43, 77)' }}
-            >
-              Email
-            </Typography>
-            <Typography>demo@example.io</Typography>
-          </div>
-          <Divider />
-          <div className={classes.details_row}>
-            <Typography
-              variant="h4"
-              component="h4"
-              style={{ color: 'rgb(23, 43, 77)' }}
-            >
-              Phone
-            </Typography>
-            <Typography>+55 748 327 439</Typography>
-          </div>
-          <Divider />
-          <div className={classes.details_row}>
-            <Typography
-              variant="h4"
-              component="h4"
-              style={{ color: 'rgb(23, 43, 77)' }}
-            >
-              Country
-            </Typography>
-            <Typography>USA</Typography>
-          </div>
-          <Divider />
-          <div className={classes.details_row}>
-            <Typography
-              variant="h4"
-              component="h4"
-              style={{ color: 'rgb(23, 43, 77)' }}
-            >
-              Address
-            </Typography>
-            <Typography>Street John Wick, no. 7</Typography>
-          </div>
+    <Card className="clinic__content">
+      <CardMedia
+        classes={{ media: "clinic__image" }}
+        component="img"
+        src="https://images.unsplash.com/photo-1576091160550-2173dba999ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
+        title="Clinic Cover Image"
+      />
+      <CardContent className="clinic__info">
+        <h6 className="clinic__name">Clinic Name Here</h6>
+        <p className="clinic__description mb-auto">Description</p>
+        <Divider />
+        <div className="clinic__action">
+          <Rating
+            name="half-rating-read"
+            value={ratingValue}
+            onChange={(event, newValue) => {
+              setRatingValue(newValue);
+            }}
+            readOnly
+          />
+          {ratingValue !== null && (
+            <Box ml={2} className="clinic__rating">
+              {ratingValue}/5
+            </Box>
+          )}
+          <Button className="ml-auto" onClick={navigateToBooking}>
+            Make An Appointment
+          </Button>
         </div>
       </CardContent>
-      <Divider />
-      <Box className={classes.button} display="flex" justifyContent="flex-end">
-        <Button onClick={handleFormSubmit} color="primary" variant="contained">
-          Save
-        </Button>
-      </Box>
+      <CardActions disableSpacing className="clinic__location">
+        <IconButton aria-label="detail location">
+          <LocationOnIcon className="mr-sm" />
+          Clinic address
+        </IconButton>
+        <IconButton
+          className={classnames("expand", {
+            "expand-open": expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse
+        className="clinic__map"
+        in={expanded}
+        timeout="auto"
+        unmountOnExit
+      >
+        <CardContent>
+          <Map />
+        </CardContent>
+      </Collapse>
     </Card>
   );
 };
--- src/pages/SearchClinicPage.tsx
+++ src/pages/SearchClinicPage.tsx
@@ -1,5 +1,38 @@
+import React from "react";
+import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import SearchIcon from "@material-ui/icons/Search";
+
+import "./SearchClinicPage.scss";
+
+import CardClinicDetail from "../components/card-clinic-detail/CardClinicDetail";
+
 const SearchClinicPage = () => {
-  return <div>Search Clinic</div>;
+  return (
+    <>
+      <section className="search-section">
+        <div className="search-bar">
+          <FormControl fullWidth>
+            <Input
+              id="input-with-icon-adornment"
+              placeholder="Search for the clinic"
+              classes={{ input: "search-input" }}
+              startAdornment={
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </div>
+      </section>
+
+      <section className="clinic-section">
+        <CardClinicDetail />
+      </section>
+    </>
+  );
 };
 
 export default SearchClinicPage;
