3f159b89f07f789a707b57fcd21f3ed248aa1a88
Khoi Nguyen <khoindct123@gmail.com>
2021-05-20 07:31:10 +0700

    commit missing files

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@material-ui/core";
+import "./CustomButton.scss";
+
+interface ICustomButton {
+  type?: "button" | "submit" | "reset" | undefined;
+  className?: string;
+  callback?: () => void;
+}
+
+const CustomButton: React.FC<ICustomButton> = ({
+  className,
+  callback,
+  children,
+  type = "button",
+}) => {
+  return (
+    <Button
+      variant="contained"
+      type={type}
+      className={className}
+      classes={{
+        contained: "button__contained",
+        label: "button__label",
+        root: "button__root",
+      }}
+      onClick={callback}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default CustomButton;
---
+++ src/pages/BookingAndReviewPage.tsx
@@ -0,0 +1,12 @@
+import CustomButton from "../components/CustomButton";
+import "./BookingAndReviewPage.scss";
+
+const BookingAndReviewPage = () => {
+  return (
+    <div className="booking-and-review-page">
+      <CustomButton>button</CustomButton>
+    </div>
+  );
+};
+
+export default BookingAndReviewPage;
