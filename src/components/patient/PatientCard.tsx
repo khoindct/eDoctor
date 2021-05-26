b15bf135403ce8da12963bedce51c9beac8558e6
Khoi Nguyen <khoindct123@gmail.com>
2021-05-27 06:23:44 +0700

    complete patient page for clinic

---
+++ src/components/patient/PatientCard.tsx
@@ -0,0 +1,85 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import "./PatientCard.scss";
+
+interface PatientCardProps {
+  patient: {
+    id: string;
+    name: string;
+    phone: string;
+    avatar: string;
+    email: string;
+    totalBookings: number;
+  };
+}
+
+const PatientCard: React.FC<PatientCardProps> = ({ patient }) => {
+  return (
+    <React.Fragment>
+      <Card className="profile">
+        <CardContent className="profile__action">
+          <div className="profile__info">
+            <div className="profile__avatar--border">
+              <Avatar
+                alt="Remy Sharp"
+                src={patient.avatar}
+                className="profile__avatar"
+              />
+            </div>
+            <Typography
+              className="profile__name"
+              variant="body2"
+              color="textSecondary"
+              component="h6"
+            >
+              {patient.name}
+            </Typography>
+          </div>
+        </CardContent>
+        <CardContent>
+          <Divider />
+          <Box mt={3} />
+          <Grid className="patient__info" container direction="column">
+            <Grid container item>
+              <h6 className="patient__title">Email</h6>
+              <Box ml="auto" />
+              <p className="patient__detail">{patient.email}</p>
+            </Grid>
+            <Grid container item>
+              <h6 className="patient__title">Phone</h6>
+              <Box ml="auto" />
+              <p className="patient__detail">{patient.phone}</p>
+            </Grid>
+            <Grid container item>
+              <h6 className="patient__title">Total Booking</h6>
+              <Box ml="auto" />
+              <p className="patient__detail">{patient.totalBookings}</p>
+            </Grid>
+          </Grid>
+        </CardContent>
+        <CardActions>
+          <Button
+            className="profile__button"
+            size="medium"
+            color="primary"
+            fullWidth
+          >
+            Details
+          </Button>
+        </CardActions>
+      </Card>
+    </React.Fragment>
+  );
+};
+
+export default PatientCard;
--- src/pages/PatientPage.tsx
+++ src/pages/PatientPage.tsx
@@ -1,19 +1,36 @@
-import { Button } from "@material-ui/core";
+import { Box, Grid } from "@material-ui/core";
 import Page from "../components/Page";
-import CustomerList from "../components/customer/CustomerList";
-import customers from "../data/customer-data";
+import PatientCard from "../components/patient/PatientCard";
 import "./PatientPage.scss";
+
+const patient = {
+  id: "1",
+  name: "Jane Rotanson",
+  phone: "0123456789",
+  avatar: "../assets/images/default-avatar.jpg",
+  email: "patient@gmail.com",
+  totalBookings: 30,
+};
 
 const PatientPage: React.FC = () => {
   return (
-    <Page className="" title="Customers">
-      <div>
-        <Button color="primary" variant="contained">
-          Add customer
-        </Button>
-      </div>
-
-      <CustomerList customers={customers} />
+    <Page className="" title="Patients">
+      <Box mt={5} />
+      <Grid
+        className="patient-dashboard-page"
+        container
+        justify="space-between"
+      >
+        <Grid item xs={12} sm={6} md={3}>
+          <PatientCard patient={patient} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <PatientCard patient={patient} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <PatientCard patient={patient} />
+        </Grid>
+      </Grid>
     </Page>
   );
 };
