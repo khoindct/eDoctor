06a3e4b0375b808f790f1c4a7d6067c350ec7c82
Khoi Nguyen <khoindct123@gmail.com>
2021-05-31 02:09:45 +0700

    commit update user password

--- src/components/CustomModal.tsx
+++ src/components/CustomModal.tsx
@@ -3,11 +3,17 @@ import { useState } from "react";
 import CustomModalBody from "./CustomModalBody";
 import "./CustomModal.scss";
 
-const CustomModal: React.FC = () => {
+interface ICustomModal {
+  type: "success" | "error";
+  message: string;
+}
+
+const CustomModal: React.FC<ICustomModal> = ({ type, message }) => {
   const [modalOpen, setModalOpen] = useState<boolean>(true);
   const handleModalClose = () => {
     setModalOpen(false);
   };
+
   return (
     <Modal
       open={modalOpen}
@@ -16,7 +22,11 @@ const CustomModal: React.FC = () => {
       aria-describedby="simple-modal-description"
       className="custom-modal"
     >
-      <CustomModalBody setModalOpen={setModalOpen} />
+      <CustomModalBody
+        setModalOpen={setModalOpen}
+        type={type}
+        message={message}
+      />
     </Modal>
   );
 };
