7fb6d10698121368f3259471f95e21f023e97bb1
Khoi Nguyen <khoindct123@gmail.com>
2021-06-21 18:54:35 +0700

    complete reply function for clinic dashboard

--- src/pages/DoctorRatingPage.tsx
+++ src/pages/DoctorRatingPage.tsx
@@ -1,15 +1,43 @@
-import { Box } from "@material-ui/core";
+import { Backdrop, Box, CircularProgress } from "@material-ui/core";
 import React from "react";
+import { useQuery } from "react-query";
+import api from "../api";
 import CommentCard from "../components/comment/CommentCard";
 import Page from "../components/Page";
+import { IReview } from "../types";
 import "./DoctorRatingPage.scss";
 
 const DoctorRatingPage: React.FC = () => {
+  const axios = api();
+
+  const getReviews = async () => {
+    const { data } = await axios.get("/reviews/clinic");
+    const result = data.data.data.reviews;
+    return result;
+  };
+
+  const { isLoading, data: reviews } = useQuery("clinicReviews", getReviews, {
+    refetchOnWindowFocus: false,
+    refetchInterval: false,
+  });
+
+  if (isLoading) {
+    return (
+      <Page className="" title="Dashboard">
+        <Backdrop className="backdrop" open>
+          <CircularProgress color="secondary" />
+        </Backdrop>
+      </Page>
+    );
+  }
+
   return (
     <Page className="" title="Rating Dashboard">
       <Box mt={5} />
       <div className="doctor-rating-page-content">
-        <CommentCard />
+        {reviews.map((review: IReview) => (
+          <CommentCard key={review._id} review={review} />
+        ))}
       </div>
       <Box mb={5} />
     </Page>
